76bd09c74948fa8ecf6a887ec2a8ce0ad475ec8c
shuyongqiang <shuyongqiang7704@dingtalk.com>
2022-11-01 16:36:47 +0800

    alter table

--- data_record.sql
+++ data_record.sql
@@ -1,6 +1,11 @@
+DROP TABLE IF EXISTS `data_record`;
 CREATE TABLE `data_record` (
   `uuid` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `byteData` blob,
-  `type` int NOT NULL,
+  `blob_data` blob,
+  `int_data` int DEFAULT NULL,
+  `short_data` smallint DEFAULT NULL,
+  `byte_data` tinyint DEFAULT NULL,
+  `boolean_data` tinyint(1) DEFAULT NULL,
+  `string_data` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
--- src/cn/test/main/mapper/DataRecordMapper.java
+++ src/cn/test/main/mapper/DataRecordMapper.java
@@ -3,32 +3,39 @@ package cn.test.main.mapper;
 import cn.test.main.domain.DataRecord;
 
 public interface DataRecordMapper {
-    /**
-     * @mbg.generated
-     */
-    int deleteByPrimaryKey(String uuid);
-    /**
-     * @mbg.generated
-     */
-    int insert(DataRecord row);
-    /**
-     * @mbg.generated
-     */
-    int insertSelective(DataRecord row);
-    /**
-     * @mbg.generated
-     */
-    DataRecord selectByPrimaryKey(String uuid);
-    /**
-     * @mbg.generated
-     */
-    int updateByPrimaryKeySelective(DataRecord row);
-    /**
-     * @mbg.generated
-     */
-    int updateByPrimaryKeyWithBLOBs(DataRecord row);
-    /**
-     * @mbg.generated
-     */
-    int updateByPrimaryKey(DataRecord row);
+
+	/**
+	 * @mbg.generated
+	 */
+	int deleteByPrimaryKey(String uuid);
+
+	/**
+	 * @mbg.generated
+	 */
+	int insert(DataRecord row);
+
+	/**
+	 * @mbg.generated
+	 */
+	int insertSelective(DataRecord row);
+
+	/**
+	 * @mbg.generated
+	 */
+	DataRecord selectByPrimaryKey(String uuid);
+
+	/**
+	 * @mbg.generated
+	 */
+	int updateByPrimaryKeySelective(DataRecord row);
+
+	/**
+	 * @mbg.generated
+	 */
+	int updateByPrimaryKeyWithBLOBs(DataRecord row);
+
+	/**
+	 * @mbg.generated
+	 */
+	int updateByPrimaryKey(DataRecord row);
 }
